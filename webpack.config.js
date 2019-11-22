40f49c52fb9908fb257590110a10932ede6c95ca
NIkola Vujanovic <nikola.vujanovic.dzonson@gmail.com>
2019-11-22 21:20:08 +0100

    Creating APP structure

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports =
+{
+    entry: "./src/js/index.js",
+    output:
+    {
+        path: path.resolve(__dirname, "dist"),
+        filename: "js/bundle.js"
+    },
+    //mode: "development"
+    plugins:
+    [
+        new HtmlWebpackPlugin
+        ({
+            filename: "index.html",
+            template: "./src/index.html"
+        })
+    ],
+    devServer:
+    {
+        contentBase: "./dist"
+    },
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
